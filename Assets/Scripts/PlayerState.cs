86111e6c846e673cb9d17726f24facf44d041459
bahriztr <bahriztr3432@gmail.com>
2024-09-17 18:51:46 +0300

    +Init

---
+++ Assets/Scripts/PlayerState.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerState
+{
+    protected Player player;
+    protected PlayerStateMachine stateMachine;
+
+    protected Rigidbody2D rb;
+
+    protected float xInput;
+
+    protected float stateTimer;
+
+    private string animBoolName;
+
+    public PlayerState(Player _player, PlayerStateMachine _stateMachine, string _animBoolName)
+    {
+        this.player = _player;
+        this.stateMachine = _stateMachine;
+        this.animBoolName = _animBoolName;
+    }
+
+    public virtual void Enter()
+    {
+        player.anim.SetBool(animBoolName, true);
+
+        rb = player.rb;
+    }
+
+    public virtual void Update()
+    {
+        stateTimer -= Time.deltaTime;
+        xInput = Input.GetAxisRaw("Horizontal");
+        player.anim.SetFloat("yVelocity", rb.velocity.y);
+    }
+
+    public virtual void Exit()
+    {
+        player.anim.SetBool(animBoolName, false);
+    }
+}
